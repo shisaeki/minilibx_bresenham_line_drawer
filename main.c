ecacff442aa72e3d1ca0289b11a10f641967e063
shisaeki <shisaeki@github.com>
2023-11-04 13:33:39 +0900

    UPDATE: input

--- main.c
+++ main.c
@@ -3,27 +3,34 @@
 
 int main()
 {
+	int	width, height;
 	int	number;
 	int	**coords;
 	t_mlx	mlx;
+	printf("Window width?:");
+	scanf("%d", &width);
+	printf("Window height?:");
+	scanf("%d", &height);
 
-	scanf("Number of lines?: %d", &number);
+	printf("Number of lines?:");
+	scanf("%d", &number);
 	coords = (int **)malloc(sizeof(int*) * number);
 	for (int i = 0; i < number; i++)
 	{
 		coords[i] = (int *)malloc(sizeof(int) * 4);
-		scanf(i + "-line start point and end point:%d%d%d%d",
-			coords[i][0],
-	     		coords[i][1],
-			coords[i][2],
-			coords[i][3]
-			);
+		printf("%d-line of start points and end point?:", i);
+		scanf("%d%d%d%d",&coords[i][0], &coords[i][1], &coords[i][2], &coords[i][3]);
 	}
 	printf("%d\n", coords[1][1]);
+
 	mlx.mlx_ptr = mlx_init();
-	mlx.win = mlx_new_window(mlx.mlx_ptr, 500, 500, "BresenhamLineDrawer");
-//	draw_line(&mlx, 100, 100, 200, 300);
-	
-//	mlx_loop(mlx.mlx_ptr);
+	mlx.win = mlx_new_window(mlx.mlx_ptr, width, height, "BresenhamLineDrawer");
+	for (int i = 0; i < number; i++)
+	{
+		draw_line(&mlx, coords[i][0], coords[i][1], coords[i][2], coords[i][3]);
+		free(coords[i]);
+	}
+	free(coords);
+	mlx_loop(mlx.mlx_ptr);
 	return (0);
 }
